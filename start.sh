98941ffbfbcdd54a26fc34b7905f6095e23fb044
Luan Santos <git@luan.sh>
2023-06-10 10:20:05 -0700

    feat(elysiera): turn runner into systemd services

--- start.sh
+++ start.sh
@@ -1,13 +1,9 @@
 #!/bin/bash -e
 
 main() {
-	if [ -d "logs" ]
-	then
-		echo -e "\e[01;32m Starting server \e[0m"
-	else
-		mkdir -p logs
-		sudo apt install -y gdb
-	fi
+	mkdir -p logs
+	mkdir -p reports/players
+	echo -e "\e[01;32m Starting server \e[0m"
 
 	ulimit -c unlimited
 	set -o pipefail
@@ -17,9 +13,8 @@ main() {
 		./update.sh
 		rm update
 	fi
-
-	./canary-current 2>&1 | awk '{ print strftime("%F %T - "), $0; fflush(); }' | tee "logs/$(date +"%F %H-%M-%S.log")"
-	echo -e "\e[0;31m Exit code $?, wait 30 seconds... \e[0m"
+		./canary-current 2>&1 | awk '{ print strftime("%F %T - "), $0; fflush(); }' | tee "logs/$(date +"%F %H-%M-%S.log")"
+		echo -e "\e[0;31m Exit code $?, wait 30 seconds... \e[0m"
 }
 
 main
