00f09ce404dec73836fe89c11f1529ff828469c0
Luan <github@luan.sh>
2023-06-19 16:52:37 -0700

    feat(elysiera): /coins command

---
+++ data-otservbr-global/scripts/talkactions/god/tibia_coins.lua
@@ -0,0 +1,40 @@
+local tibiaCoins = TalkAction("/coins")
+
+function tibiaCoins.onSay(player, words, param)
+    if not player:getGroup():getAccess() or player:getAccountType() < ACCOUNT_TYPE_GOD then
+        return true
+    end
+    local usage = "/coins PLAYER NAME, TYPE,COIN AMOUNT"
+    if param == "" then
+        player:sendCancelMessage("Command param required. Usage: ".. usage)
+        return false
+    end
+
+    local split = param:split(",")
+    if not split[3] then
+        player:sendCancelMessage("Insufficient parameters. Usage: ".. usage)
+        return false
+    end
+
+    local target = Player(split[1])
+    if not target then
+        player:sendCancelMessage("A player with that name is not online.")
+        return false
+    end
+    --trim left
+    local type = split[2]:gsub("^%s*(.-)$", "%1")
+    local amount = split[3]:gsub("^%s*(.-)$", "%1")
+
+		if type == "online" then
+			player:sendCancelMessage("Added " .. amount .. " online points to the character '" .. target:getName() .. "'.")
+			target:sendCancelMessage("Received " .. amount .. " online points!")
+			target:addTibiaCoins(tonumber(amount))
+		elseif type == "online" then
+			player:sendCancelMessage("Added " .. amount .. " elysiera coins to the character '" .. target:getName() .. "'.")
+			target:sendCancelMessage("Received " .. amount .. " elysiera coins!")
+			target:addTransferableCoins(tonumber(amount))
+		end
+end
+
+tibiaCoins:separator(" ")
+tibiaCoins:register()
