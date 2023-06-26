09abcd5bf8c82bde42ee530e1ca6790e6c4c580f
jarrodpjohnston <jarrodpjohnston@gmail.com>
2023-06-26 00:05:15 -0500

    feat(elysiera): divine missile chain

--- data-otservbr-global/scripts/spells/attack/divine_missile.lua
+++ data-otservbr-global/scripts/spells/attack/divine_missile.lua
@@ -1,19 +1,41 @@
+--combat for if target exists
 local combat = Combat()
 combat:setParameter(COMBAT_PARAM_TYPE, COMBAT_HOLYDAMAGE)
 combat:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_HOLYDAMAGE)
-combat:setParameter(COMBAT_PARAM_DISTANCEEFFECT, CONST_ANI_SMALLHOLY)
+
+
+-- combat for if target does not exist
+local combat2 = Combat()
+combat2:setParameter(COMBAT_PARAM_TYPE, COMBAT_HOLYDAMAGE)
+combat2:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_HOLYDAMAGE)
+combat2:setParameter(COMBAT_PARAM_DISTANCEEFFECT, CONST_ANI_SMALLHOLY)
 
 function onGetFormulaValues(player, level, maglevel)
 	local min = (level / 5) + (maglevel * 1.79) + 11
 	local max = (level / 5) + (maglevel * 3) + 18
 	return -min, -max
 end
+
+function onGetFormulaValues2(player, level, maglevel)
+	local min = (level / 5) + (maglevel * 1.79) + 11
+	local max = (level / 5) + (maglevel * 3) + 18
+	return -min, -max
+end
+
 combat:setCallback(CALLBACK_PARAM_LEVELMAGICVALUE, "onGetFormulaValues")
+combat2:setCallback(CALLBACK_PARAM_LEVELMAGICVALUE, "onGetFormulaValues2")
 
 local spell = Spell("instant")
 
 function spell.onCastSpell(creature, var)
-	return combat:execute(creature, var)
+	local target = Creature(var:getNumber())
+	local player = creature:getPlayer()
+	
+	if (player and target) then
+		return Chain.combat(player, target, combat, 5, 2, CONST_ANI_SMALLHOLY, CONST_ANI_SMALLHOLY)
+	else
+		return combat2:execute(creature, var)
+	end
 end
 
 spell:group("attack")
