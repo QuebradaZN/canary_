0e1bf1eb1d694eebc47579ed30dedb760bb63e5c
jarrodpjohnston <jarrodpjohnston@gmail.com>
2023-06-25 20:36:30 -0500

    feat(elysiera): single target rune buff

--- data-canary/scripts/runes/fireball.lua
+++ data-canary/scripts/runes/fireball.lua
@@ -4,8 +4,8 @@ combat:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_FIREATTACK)
 combat:setParameter(COMBAT_PARAM_DISTANCEEFFECT, CONST_ANI_FIRE)
 
 function onGetFormulaValues(player, level, maglevel)
-	local min = (level / 5) + (maglevel * 1.81) + 10
-	local max = (level / 5) + (maglevel * 3) + 18
+	local min = (level / 5) + (maglevel * 4.0) + 14
+	local max = (level / 5) + (maglevel * 7.0) + 23
 	return -min, -max
 end
 
