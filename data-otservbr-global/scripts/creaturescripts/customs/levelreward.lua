c4a8d39e0bcb9da142d465f59377c9b90fc76e4a
Luan Santos <git@luan.sh>
2023-07-18 20:43:09 -0700

    fix(elysira): prevent gaining tokens after death

--- data-otservbr-global/scripts/creaturescripts/customs/levelreward.lua
+++ data-otservbr-global/scripts/creaturescripts/customs/levelreward.lua
@@ -4,82 +4,63 @@ local table = {
 	-- [level] = type = "addon", id = {ID_ADDON_FEMALE, ID_ADDON_MALE}, msg = "MESSAGE"},
 	-- [level] = type = "mount", id = {ID_MOUNT, 0}, msg = "MESSAGE"},
 
-	[50] = {type = "bank", id = {20000, 0}, msg = "You received 20,000 gold for reaching level 50."},
-	[100] = {type = "bank", id = {50000, 0}, msg = "You received 50,000 gold for reaching level 100."},
-	[150] = {type = "bank", id = {100000, 0}, msg = "You received 100,000 gold for reaching level 150."},
-	[200] = {type = "bank", id = {200000, 0}, msg = "You received 100,000 gold for reaching level 200."},
+	[50] = { type = "bank", id = { 20000, 0 }, msg = "You received 20,000 gold for reaching level 50." },
+	[100] = { type = "bank", id = { 50000, 0 }, msg = "You received 50,000 gold for reaching level 100." },
+	[150] = { type = "bank", id = { 100000, 0 }, msg = "You received 100,000 gold for reaching level 150." },
+	[200] = { type = "bank", id = { 200000, 0 }, msg = "You received 100,000 gold for reaching level 200." },
 }
 
 local storage = Storage.Custom.LevelReward
 
+local tokens = {
+	copper = 22722,
+	iron = 22720,
+	platinum = 22723,
+	titanium = 22724,
+}
+
+local function addToken(player, token, amount)
+	local inbox = player:getSlotItem(CONST_SLOT_STORE_INBOX)
+	for _, item in pairs(inbox:getItems()) do
+		if item:getId() == token then
+			item:removeAttribute(ITEM_ATTRIBUTE_STORE)
+		end
+	end
+
+	inbox:addItem(token, amount, INDEX_WHEREEVER, FLAG_NOLIMIT)
+
+	for _, item in pairs(inbox:getItems()) do
+		if item:getId() == token then
+			item:setAttribute(ITEM_ATTRIBUTE_STORE, systemTime())
+		end
+	end
+end
+
 local levelReward = CreatureEvent("Level Reward")
 function levelReward.onAdvance(player, skill, oldLevel, newLevel)
-	if skill ~= SKILL_LEVEL or newLevel <= oldLevel then
-		return true
-	end
-
-	local inbox = player:getSlotItem(CONST_SLOT_STORE_INBOX)
-	local inboxItem
-
-	if newLevel >= 50 and newLevel < 100 then
-		local inboxItem = player:findItemInInbox(22722)
-		
-		if inboxItem then
-			inboxItem:removeAttribute(ITEM_ATTRIBUTE_STORE)
-		end
-
-		inboxItem = inbox:addItem(22722, 5, INDEX_WHEREEVER, FLAG_NOLIMIT)
-		inboxItem = player:findItemInInbox(22722)
-		inboxItem:setAttribute(ITEM_ATTRIBUTE_STORE, systemTime())
-	end
-
-	if newLevel >= 100 and newLevel < 150 then
-		local inboxItem = player:findItemInInbox(22720)
-		
-		if inboxItem then
-			inboxItem:removeAttribute(ITEM_ATTRIBUTE_STORE)
-		end
-
-		inboxItem = inbox:addItem(22720, 5, INDEX_WHEREEVER, FLAG_NOLIMIT)
-		inboxItem = player:findItemInInbox(22720)
-		inboxItem:setAttribute(ITEM_ATTRIBUTE_STORE, systemTime())
-	end
-
-	if newLevel >= 150 and newLevel < 200 then
-		local inboxItem = player:findItemInInbox(22723)
-		
-		if inboxItem then
-			inboxItem:removeAttribute(ITEM_ATTRIBUTE_STORE)
-		end
-
-		inboxItem = inbox:addItem(22723, 5, INDEX_WHEREEVER, FLAG_NOLIMIT)
-		inboxItem = player:findItemInInbox(22723)
-		inboxItem:setAttribute(ITEM_ATTRIBUTE_STORE, systemTime())
-	end
-
-	if newLevel >= 200 then
-		local inboxItem = player:findItemInInbox(22724)
-		
-		if inboxItem then
-			inboxItem:removeAttribute(ITEM_ATTRIBUTE_STORE)
-		end
-
-		inboxItem = inbox:addItem(22724, 5, INDEX_WHEREEVER, FLAG_NOLIMIT)
-		inboxItem = player:findItemInInbox(22724)
-		inboxItem:setAttribute(ITEM_ATTRIBUTE_STORE, systemTime())
-	end
+	local previousLevel = player:getStorageValue(storage)
+	if skill ~= SKILL_LEVEL or newLevel <= oldLevel then return true end
+	if previousLevel >= newLevel then return true end
 
 	if newLevel >= 20 and player:getStorageValue(STORAGEVALUE_PROMOTION) ~= 1 then
 		if player:getVocation():getPromotion() ~= nil then
-			player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "Congratulations on reaching level 20. You have been automatically promoted to " .. player:getVocation():getPromotion():getName() .. ".")
+			player:sendTextMessage(MESSAGE_EVENT_ADVANCE,
+				"Congratulations on reaching level 20. You have been automatically promoted to " ..
+				player:getVocation():getPromotion():getName() .. ".")
 			player:setVocation(player:getVocation():getPromotion())
 			player:setStorageValue(STORAGEVALUE_PROMOTION, 1)
 			player:getPosition():sendMagicEffect(CONST_ME_HOLYDAMAGE)
 		end
 	end
 
+	local levelsGained = newLevel - math.max(previousLevel, oldLevel)
+	if newLevel >= 50 and newLevel < 100 then addToken(player, tokens.copper, 5 * levelsGained)
+	elseif newLevel >= 100 and newLevel < 150 then addToken(player, tokens.iron, 5 * levelsGained)
+	elseif newLevel >= 150 and newLevel < 200 then addToken(player, tokens.platinum, 5 * levelsGained)
+	elseif newLevel >= 200 then addToken(player, tokens.titanium, 5 * levelsGained) end
+
 	for level, _ in pairs(table) do
-		if newLevel >= level and player:getStorageValue(storage) < level then
+		if newLevel >= level then
 			if table[level].type == "item" then
 				player:addItem(table[level].id[1], table[level].id[2])
 			elseif table[level].type == "bank" then
