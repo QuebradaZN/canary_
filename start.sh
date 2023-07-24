2baf1fd4e264c3ec02edd8754ef3aaa4105d8635
Luan Santos <git@luan.sh>
2023-07-24 15:48:16 -0700

    deploy(elysiera): gdb

--- start.sh
+++ start.sh
@@ -13,8 +13,10 @@ main() {
 		./update.sh
 		rm update
 	fi
-		./canary-current 2>&1 | awk '{ print strftime("%F %T - "), $0; fflush(); }' | tee "logs/$(date +"%F %H-%M-%S.log")"
+
+		gdb --batch -return-child-result --command=gdb_debug --args ./canary-current 2>&1 | awk '{ print strftime("%F %T - "), $0; fflush(); }' | tee "logs/$(date +"%F %H-%M-%S.log")"
 		echo -e "\e[0;31m Exit code $?, wait 30 seconds... \e[0m"
+		sleep 30
 }
 
 main
