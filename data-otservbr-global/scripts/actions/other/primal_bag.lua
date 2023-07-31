c807a5b3897b79a2ba4dbe4e3754d7cb99299391
Luan Santos <git@luan.sh>
2023-07-30 23:32:26 -0700

    feat(elysiera): vocation based rewards
    Closes ELY-76

--- data-otservbr-global/scripts/actions/other/primal_bag.lua
+++ data-otservbr-global/scripts/actions/other/primal_bag.lua
@@ -1,23 +1,34 @@
 local rewards = {
-    { id = 39147, name = "spiritthorn armor" },
-    { id = 39148, name = "spiritthorn helmet" },
-    { id = 39149, name = "alicorn headguard" },
-    { id = 39150, name = "alicorn quiver" },
-    { id = 39151, name = "arcanomancer regalia" },
-    { id = 39152, name = "arcanomancer folio" },
-    { id = 39153, name = "arboreal crown" },
-    { id = 39154, name = "arboreal tome" },
-    { id = 39177, name = "charged spiritthorn ring" },
-    { id = 39180, name = "charged alicorn ring" },
-    { id = 39183, name = "charged arcanomancer sigil" },
-    { id = 39186, name = "charged arboreal ring" }
+	[VOCATION.BASE_ID.KNIGHT] = {
+		{ id = 39147, name = "spiritthorn armor" },
+		{ id = 39148, name = "spiritthorn helmet" },
+		{ id = 39177, name = "charged spiritthorn ring" },
+	},
+	[VOCATION.BASE_ID.PALADIN] = {
+		{ id = 39149, name = "alicorn headguard" },
+		{ id = 39150, name = "alicorn quiver" },
+		{ id = 39180, name = "charged alicorn ring" },
+	},
+	[VOCATION.BASE_ID.SORCERER] = {
+		{ id = 39151, name = "arcanomancer regalia" },
+		{ id = 39152, name = "arcanomancer folio" },
+		{ id = 39183, name = "charged arcanomancer sigil" },
+	},
+	[VOCATION.BASE_ID.DRUID] = {
+		{ id = 39153, name = "arboreal crown" },
+		{ id = 39154, name = "arboreal tome" },
+		{ id = 39186, name = "charged arboreal ring" }
+	},
 }
 
 local primalBag = Action()
 
 function primalBag.onUse(player, item, fromPosition, target, toPosition, isHotkey)
-	local randId = math.random(1, #rewards)
-	local rewardItem = rewards[randId]
+	if not player then return false end
+	local vocationRewards = rewards[player:getVocation():getBaseId()]
+
+	local randId = math.random(1, #vocationRewards)
+	local rewardItem = vocationRewards[randId]
 
 	player:addItem(rewardItem.id, 1)
 	item:remove(1)
