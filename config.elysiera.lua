38e14a3c25c74ef17dbebc30f1417649f3a7290f
Luan Santos <git@luan.sh>
2023-08-02 13:16:42 -0700

    config(elysiera): move serversave to 7PM

--- config.elysiera.lua
+++ config.elysiera.lua
@@ -294,7 +294,7 @@ globalServerSaveNotifyDuration = 5
 globalServerSaveCleanMap = false
 globalServerSaveClose = false
 globalServerSaveShutdown = true
-globalServerSaveTime = "23:55:00"
+globalServerSaveTime = "1:55:00"
 
 -- Sort loot by chance, most rare items drop first
 -- it is good to be setted when you have a higher
