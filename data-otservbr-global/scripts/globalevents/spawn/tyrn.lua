7ffa07740d6bfee64831c4c6102783731b7a7f21
Kauana Santos <kauana@hey.com>
2023-08-06 12:03:11 -0700

    fix tyrn spawn location

--- data-otservbr-global/scripts/globalevents/spawn/tyrn.lua
+++ data-otservbr-global/scripts/globalevents/spawn/tyrn.lua
@@ -1,7 +1,7 @@
 local config = {
     monsterName = 'Tyrn',
-    bossPosition = Position(33094, 32404, 13),
-    centerPosition = Position(33094, 32404, 13),
+    bossPosition = Position(33056, 32393, 14),
+    centerPosition = Position(33056, 32393, 14),
     rangeX = 50,
     rangeY = 50
 }
