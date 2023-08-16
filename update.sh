1f8ef19d3f02592f0887f1e5a63b98701b22b840
Luan Santos <github@luan.sh>
2023-08-15 22:38:07 -0700

    init(elysiera): test server configs

--- update.sh
+++ update.sh
@@ -5,12 +5,15 @@ main() {
 	git lfs pull
 
 	source ~/.profile
+	local branch=$(git rev-parse --abbrev-ref HEAD)
+	local commit=$(git rev-parse HEAD)
+	local artifact="canary-linux-release-${branch}-${commit}"
 
   url=$(curl -sL \
     -H "Accept: application/vnd.github+json" \
     -H "Authorization: Bearer $GITHUB_TOKEN"\
     -H "X-GitHub-Api-Version: 2022-11-28" \
-    https://api.github.com/repos/elysiera/canary/actions/artifacts?per_page=1 | jq -r '.artifacts[0].archive_download_url')
+    https://api.github.com/repos/elysiera/canary/actions/artifacts?name="${artifact}"per_page=1 | jq -r '.artifacts[0].archive_download_url')
   curl -Lo canary.zip \
     -H "Accept: application/vnd.github+json" \
     -H "Authorization: Bearer $GITHUB_TOKEN"\
