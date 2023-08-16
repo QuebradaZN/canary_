2a677232606ffb2f9c6d8cae6578f4b8e527cb58
Luan Santos <github@luan.sh>
2023-08-15 23:22:46 -0700

    fix(elysiera): download URL

--- update.sh
+++ update.sh
@@ -13,7 +13,7 @@ main() {
 		-H "Accept: application/vnd.github+json" \
 		-H "Authorization: Bearer $GITHUB_TOKEN"\
 		-H "X-GitHub-Api-Version: 2022-11-28" \
-		https://api.github.com/repos/elysiera/canary/actions/artifacts?name="${artifact}"per_page=1 | jq -r '.artifacts[0].archive_download_url')
+		"https://api.github.com/repos/elysiera/canary/actions/artifacts?name=${artifact}&per_page=1" | jq -r '.artifacts[0].archive_download_url')
 	if [ -z "$url" ] || [ "$url" = "null" ]; then
 		echo "No artifact found for ${artifact}"
 		exit 1
