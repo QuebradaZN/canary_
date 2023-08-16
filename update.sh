6e062f091523e5c87e7bc11d3b0678704ad00cd5
Luan Santos <github@luan.sh>
2023-08-15 23:13:37 -0700

    deploy(elysiera): validate url

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 main() {
-  git pull
+	git pull
 	git lfs pull
 
 	source ~/.profile
@@ -9,27 +9,27 @@ main() {
 	local commit=$(git rev-parse HEAD)
 	local artifact="canary-linux-release-${branch}-${commit}"
 
-  url=$(curl -sL \
-    -H "Accept: application/vnd.github+json" \
-    -H "Authorization: Bearer $GITHUB_TOKEN"\
-    -H "X-GitHub-Api-Version: 2022-11-28" \
-    https://api.github.com/repos/elysiera/canary/actions/artifacts?name="${artifact}"per_page=1 | jq -r '.artifacts[0].archive_download_url')
-	if [ -z "$url" ]; then
+	url=$(curl -sL \
+		-H "Accept: application/vnd.github+json" \
+		-H "Authorization: Bearer $GITHUB_TOKEN"\
+		-H "X-GitHub-Api-Version: 2022-11-28" \
+		https://api.github.com/repos/elysiera/canary/actions/artifacts?name="${artifact}"per_page=1 | jq -r '.artifacts[0].archive_download_url')
+	if [ -z "$url" ] || [ "$url" = "null" ]; then
 		echo "No artifact found for ${artifact}"
 		exit 1
 	fi
 	echo "Downloading ${url}..."
-  curl -Lo canary.zip \
-    -H "Accept: application/vnd.github+json" \
-    -H "Authorization: Bearer $GITHUB_TOKEN"\
-    -H "X-GitHub-Api-Version: 2022-11-28" \
-    "${url}"
-  unzip canary.zip
-  rm -f canary.zip
-  chmod a+x canary
-  rm -f canary-previous
-  mv canary-current canary-previous || true
-  mv canary canary-current || true
+	curl -Lo canary.zip \
+		-H "Accept: application/vnd.github+json" \
+		-H "Authorization: Bearer $GITHUB_TOKEN"\
+		-H "X-GitHub-Api-Version: 2022-11-28" \
+		"${url}"
+	unzip canary.zip
+	rm -f canary.zip
+	chmod a+x canary
+	rm -f canary-previous
+	mv canary-current canary-previous || true
+	mv canary canary-current || true
 }
 
 main
