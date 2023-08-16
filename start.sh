309be45c7a0b0ef0ea351a9a8b92a0258ce1ae2d
Luan Santos <github@luan.sh>
2023-08-15 23:07:56 -0700

    deploy(elysiera): always update

--- start.sh
+++ start.sh
@@ -8,11 +8,8 @@ main() {
 	ulimit -c unlimited
 	set -o pipefail
 
-	if [ -f update ]; then
-		echo -e "\e[01;32m Updating server \e[0m"
-		./update.sh
-		rm update
-	fi
+	echo -e "\e[01;32m Updating server \e[0m"
+	./update.sh
 
 	gdb --batch -return-child-result --command=gdb_debug --args ./canary-current 2>&1 | awk '{ print strftime("%F %T - "), $0; fflush(); }' | tee "logs/$(date +"%F %H-%M-%S.log")"
 	echo -e "\e[0;31m Exit code $?, wait 30 seconds... \e[0m"
