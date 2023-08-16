2fefe33132b4256edbb943e1924c2382ba35255c
Luan Santos <github@luan.sh>
2023-08-15 23:14:22 -0700

    fix(elysiera): refuse to start if update fails

--- start.sh
+++ start.sh
@@ -9,7 +9,7 @@ main() {
 	set -o pipefail
 
 	echo -e "\e[01;32m Updating server \e[0m"
-	./update.sh
+	./update.sh || exit 1
 
 	gdb --batch -return-child-result --command=gdb_debug --args ./canary-current 2>&1 | awk '{ print strftime("%F %T - "), $0; fflush(); }' | tee "logs/$(date +"%F %H-%M-%S.log")"
 	echo -e "\e[0;31m Exit code $?, wait 30 seconds... \e[0m"
