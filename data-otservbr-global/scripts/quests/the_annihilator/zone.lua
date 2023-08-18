a866fee5e3448bae00210ea2e8d787454b458780
Luan Santos <git@luan.sh>
2023-08-17 23:03:03 -0700

    feat: prevent familiars from getting into annihilator

---
+++ data-otservbr-global/scripts/quests/the_annihilator/zone.lua
@@ -0,0 +1,12 @@
+local zone = Zone("annihilator")
+
+zone:addArea(Position(33217, 31654, 13), Position(33238, 31661, 13))
+
+local event = ZoneEvent(zone)
+function event.onEnter(_zone, creature)
+	local monster = creature:getMonster()
+	return not (monster and monster:getMaster() and monster:getMaster():isPlayer())
+end
+
+event:register()
+zone:register()
