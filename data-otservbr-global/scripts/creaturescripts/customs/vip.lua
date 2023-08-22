df82a899c30a7c157d3fdba9696042cc095ddb15
Luan Santos <git@luan.sh>
2023-08-22 16:44:28 -0700

    fix: bad vip merge

--- data-otservbr-global/scripts/creaturescripts/customs/vip.lua
+++ data-otservbr-global/scripts/creaturescripts/customs/vip.lua
@@ -5,9 +5,7 @@ function playerLogin.onLogin(player)
 		local wasVip = player:getStorageValue(Storage.VipSystem.IsVip) == 1
 		if wasVip and not player:isVip() then player:onRemoveVip() end
 		if player:isVip() then player:onAddVip(player:getVipDays(), wasVip) end
-
-			CheckPremiumAndPrint(player, MESSAGE_LOGIN)
-		end
+		CheckPremiumAndPrint(player, MESSAGE_LOGIN)
 	end
 	return true
 end
