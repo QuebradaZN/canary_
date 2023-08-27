43a31ff9e18b6530f9e701ff9639b28ea685ce8b
Luan Santos <git@luan.sh>
2023-08-26 17:32:12 -0700

    fix: bad test merge

--- tests/stubs/in_memory_logger.hpp
+++ tests/stubs/in_memory_logger.hpp
@@ -17,57 +17,57 @@
 namespace di = boost::di;
 
 class InMemoryLogger : public Logger {
-private:
-	struct LogEntry {
-		std::string level;
-		std::string message;
-	};
+	private:
+		struct LogEntry {
+			std::string level;
+			std::string message;
+		};
 
-public:
-	mutable std::vector<LogEntry> logs;
+	public:
+		mutable std::vector<LogEntry> logs;
 
-	static di::extension::injector<> &install(di::extension::injector<> &injector) {
-		injector.install(di::bind<Logger>.to<InMemoryLogger>().in(di::singleton));
-		return injector;
-	}
+		static di::extension::injector<> &install(di::extension::injector<> &injector) {
+			injector.install(di::bind<Logger>.to<InMemoryLogger>().in(di::singleton));
+			return injector;
+        }
 
-	bool hasLogEntry(const std::string &lvl, const std::string &expectedMsg) const {
-		for (const auto &entry : logs) {
-			if (entry.level == lvl && entry.message == expectedMsg) {
-				return true;
+		bool hasLogEntry(const std::string& lvl, const std::string& expectedMsg) const {
+			for (const auto& entry : logs) {
+				if (entry.level == lvl && entry.message == expectedMsg) {
+					return true;
+				}
 			}
+
+			return false;
 		}
 
-		return false;
-	}
-
-	void setLevel(const std::string &name) override {
-		// For the stub, setting a level might not have any behavior.
-		// But you can implement level filtering if you like.
-	}
-
-	[[nodiscard]] std::string getLevel() const override {
-		// For simplicity, let's just return a default level. You can adjust as needed.
-		return "DEBUG";
-	}
-
-	virtual void log(std::string lvl, fmt::basic_string_view<char> msg) const override {
-		logs.push_back({ lvl, { msg.data(), msg.size() } });
-	}
-
-	// Helper methods for testing
-	size_t logCount() const {
-		return logs.size();
-	}
-
-	std::pair<std::string, std::string> getLogEntry(size_t index) const {
-		if (index < logs.size()) {
-			return { logs[index].level, logs[index].message };
+		void setLevel(const std::string &name) override {
+			// For the stub, setting a level might not have any behavior.
+			// But you can implement level filtering if you like.
 		}
-		return { "", "" }; // Return empty pair for out-of-bounds. Alternatively, you could throw an exception.
-	}
 
-	void clearLogs() {
-		logs.clear();
-	}
+		[[nodiscard]] std::string getLevel() const override {
+			// For simplicity, let's just return a default level. You can adjust as needed.
+			return "DEBUG";
+		}
+
+		virtual void log(std::string lvl, fmt::basic_string_view<char> msg) const override {
+			logs.push_back({lvl, {msg.data(), msg.size()}});
+		}
+
+		// Helper methods for testing
+		size_t logCount() const {
+			return logs.size();
+		}
+
+		std::pair<std::string, std::string> getLogEntry(size_t index) const {
+			if (index < logs.size()) {
+				return {logs[index].level, logs[index].message};
+			}
+			return {"", ""}; // Return empty pair for out-of-bounds. Alternatively, you could throw an exception.
+		}
+
+		void clearLogs() {
+			logs.clear();
+		}
 };
